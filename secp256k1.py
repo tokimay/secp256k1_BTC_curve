cca9e96a185188151056cdc04dec9db2e0743590
tokimay <tokimay@gmail.com>
2025-02-25 13:05:07 +0330

    License 403.12

--- secp256k1.py
+++ secp256k1.py
@@ -1,4 +1,22 @@
 
+# This file is part of https://github.com/tokimay/secp256k1_BTC_curve
+# Copyright (C) 2016 https://github.com/tokimay
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# This software is licensed under GPLv3. If you use or modify this project,
+# you must include a reference to the original repository: https://github.com/tokimay/secp256k1_BTC_curve
+
 __A = 0
 __B = 7
 __P = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
